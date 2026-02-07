a663a990f53a2a163beab24e01a86c1c3b45ab20
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2026-02-07 21:04:27 +0600

    feature setup postgres db & user model

---
+++ backend/app/db/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# SQLAlchemy Engine তৈরি
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# DB Session পাওয়ার জন্য ডিপেন্ডেন্সি
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
